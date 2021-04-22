70698a711af3f9f874f13404bb3f419d646fdb8a
Ionut Hada <hadaionut@gmail.com>
2021-04-22 21:47:02 +0300

    Share function, persistancy for locking

--- scripts/MediaDisplayApp.js
+++ scripts/MediaDisplayApp.js
@@ -1,17 +1,28 @@
+const shareImage = async (imgPath, text, actorId, type) => {
+    await game.socket.emit('module.ShowArt', {
+        imgPath: imgPath,
+        text: text,
+        actorId: actorId,
+        type: type
+    })
+}
+
 export default class MediaDisplayApp extends FormApplication {
-    constructor (imageLocation, text, actor, type) {
+    constructor (imageLocation, text, actor, type, playerIsGM) {
         super();
         this.imageLocation = imageLocation;
         this.text = text || '';
         this.actor = actor;
         this.type = type;
+        this.playerIsGm = playerIsGM;
+        this.currentState = actor.getFlag('ShowArt', 'visibility');
     }
 
     static get defaultOptions() {
         return {
             ...super.defaultOptions,
             id: "show-art-media-display-app",
-            template: "modules/Show-Art/templates/mediaDisplayApp.hbs",
+            template: "modules/ShowArt/templates/mediaDisplayApp.hbs",
             resizable: true,
             minimizable: false,
             submitOnClose: true,
@@ -24,12 +35,42 @@ export default class MediaDisplayApp extends FormApplication {
     getData(options) {
         return {
             imageLocation: this.imageLocation,
-            text: this.text
+            text: this.text,
+            isGM: this.playerIsGm && (this.currentState === 'hidden' || this.currentState === undefined)
         }
     }
 
     async _updateObject(event, formData) {
-        await this.actor.setFlag('Show-Art', `Author-${this.type}`, formData.author);
-        console.log(formData);
+        if (this.playerIsGm) {
+            await this.actor.setFlag('ShowArt', `Author-${this.type}`, formData.author);
+            await this.actor.setFlag('ShowArt', 'visibility', this.currentState);
+        }
+
+    }
+
+    createSaveButton(appendLocation, html) {
+        if (!game.user.isGM) return;
+
+        const hideInputButton = $(`<a> <i class="fas fa-save"> Lock</i></a>`);
+        hideInputButton.on('click',(event) => {
+            const input = html.find('.author-input');
+            const selected = html.find('.author-selected');
+            input.toggle();
+            selected.toggle();
+            selected.children('p')[0].innerText = `Author: ${html.find('.author-input').children('input').val()}`;
+            this.currentState = this.currentState === 'visible' ? 'hidden' : 'visible';
+        });
+        appendLocation.before(hideInputButton);
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        const appendLocation = html.parent().parent().find('.close');
+        const shareButton = $(`<a> <i class="fas fa-share-square"></i> Share</a>`);
+        shareButton.on('click', () => shareImage(this.imageLocation, this.text, this.actor.data._id, this.type));
+        appendLocation.before(shareButton);
+
+        this.createSaveButton(appendLocation, html);
     }
 }
--- scripts/module.js
+++ scripts/module.js
@@ -4,9 +4,6 @@ Hooks.once('init', async () => {
     CONFIG.debug.hooks = true;
 });
 
-Hooks.once('ready', async () => {
-});
-
 const createButton = () => {
     let button = document.createElement("div");
 
@@ -23,11 +20,21 @@ Hooks.on('renderTokenHUD', (hud, html, title) => {
     const actor = game.actors.get(title.actorId)
 
     artButton.on('click', () => {
-        new MediaDisplayApp(title.img, actor.getFlag('Show-Art', 'Author-token'), actor, 'token').render(true);
+        new MediaDisplayApp(title.img, actor.getFlag('ShowArt', 'Author-token'), actor, 'token', game.user.isGM).render(true);
     });
     artButton.on('contextmenu', () => {
-        new MediaDisplayApp(actor.data.img, actor.getFlag('Show-Art', 'Author-main'), actor, 'main').render(true);
+        new MediaDisplayApp(actor.data.img, actor.getFlag('ShowArt', 'Author-main'), actor, 'main', game.user.isGM).render(true);
     })
 
     html.children('.left').append(artButton);
+})
+
+const receiveSharedImages = (receivedObject) => {
+    console.log('received image');
+    new MediaDisplayApp(receivedObject.imgPath, receivedObject.text, game.actors.get(receivedObject.actorId), receivedObject.type, game.user.isGM).render(true);
+}
+
+Hooks.on("ready", () => {
+    console.log('setting up socket connection');
+    game.socket.on('module.ShowArt', receiveSharedImages);
 })
